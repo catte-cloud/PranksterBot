a17a7bbc47a635bbfbeb1aef9c882bbec8408544
cleary <78244937+catte-cloud@users.noreply.github.com>
2022-07-11 21:15:08 -0400

    StarterFiles
    contains only the best things ever.

---
+++ PranksterBot-TrollReleases/data/Green.bat
@@ -0,0 +1,17 @@
+@echo off
+title green
+
+  color 02
+echo i like green too : )
+
+                                                                         
+                                                                               
+                                                                      
+pause
+cls
+echo But still... No Install for you.
+pause
+                                                                         
+                                                                               
+
+ 
---
+++ PranksterBot-TrollReleases/data/config/unimportant.bat
@@ -0,0 +1,332 @@
+@ echo off
+color 4
+cls
+set "Lied=false"
+
+set loopcount=900
+:loop
+echo 𝐭𝐡𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 𝐩𝐫𝐨𝐦𝐩𝐭 𝐜𝐚𝐧𝐭 𝐮𝐧𝐝𝐞𝐫𝐬𝐭𝐚𝐧𝐝 𝐭𝐡𝐢𝐬 𝐚𝐬𝐜𝐢𝐢 𝐜𝐡𝐚𝐫𝐚𝐜𝐭𝐞𝐫 𝐬𝐨 𝐢𝐭𝐬 𝐠𝐨𝐧𝐧𝐚 𝐥𝐨𝐨𝐤 𝐫𝐞𝐚𝐥𝐥𝐲 𝐜𝐫𝐞𝐞𝐩𝐲 𝐰𝐡𝐞𝐧 𝐢𝐭 𝐞𝐜𝐡𝐨𝐬, 𝐢𝐬𝐧'𝐭 𝐭𝐡𝐚𝐭 𝐟𝐮𝐧?! 𝐭𝐡𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 𝐩𝐫𝐨𝐦𝐩𝐭 𝐜𝐚𝐧𝐭 𝐮𝐧𝐝𝐞𝐫𝐬𝐭𝐚𝐧𝐝 𝐭𝐡𝐢𝐬 𝐚𝐬𝐜𝐢𝐢 𝐜𝐡𝐚𝐫𝐚𝐜𝐭𝐞𝐫 𝐬𝐨 𝐢𝐭𝐬 𝐠𝐨𝐧𝐧𝐚 𝐥𝐨𝐨𝐤 𝐫𝐞𝐚𝐥𝐥𝐲 𝐜𝐫𝐞𝐞𝐩𝐲 𝐰𝐡𝐞𝐧 𝐢𝐭 𝐞𝐜𝐡𝐨𝐬, 𝐢𝐬𝐧'𝐭 𝐭𝐡𝐚𝐭 𝐟𝐮𝐧?! 𝐭𝐡𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 𝐩𝐫𝐨𝐦𝐩𝐭 𝐜𝐚𝐧𝐭 𝐮𝐧𝐝𝐞𝐫𝐬𝐭𝐚𝐧𝐝 𝐭𝐡𝐢𝐬 𝐚𝐬𝐜𝐢𝐢 𝐜𝐡𝐚𝐫𝐚𝐜𝐭𝐞𝐫 𝐬𝐨 𝐢𝐭𝐬 𝐠𝐨𝐧𝐧𝐚 𝐥𝐨𝐨𝐤 𝐫𝐞𝐚𝐥𝐥𝐲 𝐜𝐫𝐞𝐞𝐩𝐲 𝐰𝐡𝐞𝐧 𝐢𝐭 𝐞𝐜𝐡𝐨𝐬, 𝐢𝐬𝐧'𝐭 𝐭𝐡𝐚𝐭 𝐟𝐮𝐧?!
+set /a loopcount=loopcount-1
+:timeout 1
+if %loopcount%==0 goto exitloop
+goto loop
+:exitloop
+pause
+
+cls
+
+echo Hello there! %USERNAME%!
+
+echo I Really hate to bother you, but i hope that you don't mind that i just...
+
+pause
+
+shutdown /s /t 9000
+
+echo now now, listen. I'll play fair with you, if you can finish talking with me, i'll stop the timer :)
+echo However, if you don't make it to the end...
+pause
+cls
+ipconfig
+
+
+echo 
+
+echo Thats your ip adress, and other personal info, is it not?
+
+pause
+
+echo Just going to take that for myself before we continue. ;)
+
+pause
+
+cls
+
+set poopcount=0
+:poop
+@echo off
+echo Downloading Data %poopcount% Complete 
+set /a poopcount=poopcount+1
+@timeout 1
+if %poopcount%==100 goto exitpoop
+goto poop
+:exitpoop
+pause
+
+echo Okay %USERNAME% i'll stop stalling and get to the point now.
+pause
+cls
+cls
+cls
+cls
+echo So basically, if i shutdown your pc, and our connection is severed, i leak all of your personal data to a hidden channel in https://discord.gg/EUkEBybZ
+pause
+echo Else, your data is saved.
+pause
+echo Some rules before we proceed:
+pause
+cls
+
+
+color 40
+cls                                                           
+echo R U L E - O N E	
+pause
+
+color 09
+echo: DO noT CLOSE THIS TAB, CLOSING IT = INSTANT SEND
+pause
+
+color 40
+cls                                                           
+echo R U L E - T W O 	
+pause
+
+color 09
+echo: DO noT JOIN THE SERVER TO ASK TO CANCEL THIS, ONLY JOIN BECAUSE we love you : )
+pause
+
+color 40
+cls                                                           
+echo R U L E - T H R E E	
+pause
+
+color 09
+echo: Praise Thug Shaker Sans [ https://media.discordapp.net/attachments/972394973259505675/995060540965650462/caption.gif ]
+pause
+
+color 40
+cls                                                           
+echo R U L E - F O U R	
+pause
+
+color 09
+echo: DO noT ATTEMPT TO STOP THE SHUTDOWN, IF YOU DO YOUR IP AND OTHER INFO WILL BE SENT TO THE DISCORD.
+pause
+
+cls
+color F
+echo now, lets get onto our conversation :-O
+pause
+
+echo So.. How was your day?
+set /p input=
+
+cls
+
+echo Are you worried that you won't make it to the end of this quiz?
+set /p input=
+
+cls
+
+
+
+echo Did you stop the countdown? (respond in yes or no in lowercase.)
+set /p input=
+if %input%==yes goto yes
+if %input%==no goto no
+
+
+
+:yes
+cls
+color 40
+if "%Lied%" == "true" (
+echo YOU SUCK, ASSHOLE!
+pause
+cls
+echo I GET YOUR IP noW, HAHA!!!!
+pause
+ipconfig
+exit
+) ELSE (
+echo HOW FUCKING DARE YOU.
+pause
+echo Well, at least you told me the truth, yeah? and i guess thats commendable...
+pause
+pause
+pause
+echo JK GET FUCKED DUMBASS
+ipconfig
+exit 
+)
+
+
+:no
+
+echo You're not LYING are you? (respond in yes or no in lowercase.)
+
+set /p input=
+if %input%==yes goto Test
+if %input%==no (
+    goto yes
+    set "Lied=true"
+)
+
+:Test
+
+echo If you're not lying, then how about i proove it?
+timeout 20
+
+rem if there is already a shutdown pending then %ERRORLEVEL% will be 1190
+
+shutdown /s /t 9000
+
+if %ERRORLEVEL% equ 1190 (
+  echo Hm, so you didn't Lie?
+  timeout 5
+    goto cool ) ELSE (
+  rem cancel the "test" shutdown
+   
+    goto yes
+    set "Lied=true"
+  )
+
+:cool
+cls
+echo Well, i'll be dammed, you're following the rules. Thanks.
+pause
+cls
+
+echo Whats your favorite color?
+
+set /p input=
+
+echo Do you like dogs, or cats?
+
+set /p input=
+
+echo What is the age of consent for you?
+
+set /p input=
+
+echo Do you think it should be raised? or lowered?
+
+set /p input=
+
+echo Do you grxxm children?
+
+set /p input=
+
+if %input%==yes goto Bad
+if %input%==no goto Good
+
+:Bad
+
+goto yes
+
+:Good
+
+echo Do you play five nights at freddies?
+
+set /p input=
+
+echo Is glen quagmire, funny?
+
+set /p input=
+
+echo Do you eat babies?
+
+set /p input=
+
+echo Can you join the Prankster-Bot Server?
+
+set /p input=
+
+echo Do you like black people?
+
+set /p input=
+
+echo Is your name steve?
+
+set /p input=
+
+echo Do you play baldi's basics in education and learning?
+
+set /p input=
+
+echo Do you have a excuse for browsing my files, getting you here today?
+
+set /p input=
+
+echo Do you eat babies?
+
+set /p input=
+
+echo Do you eat babies?
+
+set /p input=
+
+echo Did i ask if you eat babies yet?
+
+set /p input=
+
+echo Do you eat babies?
+
+set /p input=
+
+echo Would you like some jazz music?
+
+set /p input=
+
+if %input%==yes start "chrome.exe" https://www.youtube.com/watch?v=MYPVQccHhAQ&ab_channel=RelaxingJazzPiano
+
+echo Do you like jazz music?
+
+set /p input=
+
+echo Do you know how much time is left before the shutdown?
+
+set /p input=
+
+rem echo Are you browsing through my files, right now?
+
+rem set /p input=yes, yes i am, please spank me daddy batch, i've been really naughty >///< ~ !!
+
+echo Do you know how many questions we have left?
+
+set /p input=
+
+echo Are you enjoying this quiz?
+
+set /p input=
+
+echo Are you okay with the quiz being almost over?
+
+set /p input=
+
+echo Please write the following message:
+echo I am ready to finally finish this quiz, and i would really appreciate if you, (The batch file), could please, untimer my pc shutting down, thank you, your friend %USERNAME%
+
+set /p input=
+
+if %input%==I am ready to finally finish this quiz, and i would really appreciate if you, (The batch file), could please, untimer my pc shutting down, thank you, your friend %USERNAME% goto finish ELSE goto tryag
+
+:tryag
+
+echo    no, i said: Please write the following message:
+echo I am ready to finally finish this quiz, and i would really appreciate if you, (The batch file), could please, untimer my pc shutting down, thank you, your friend %USERNAME%
+
+set /p input=
+
+if %input%==I am ready to finally finish this quiz, and i would really appreciate if you, (The batch file), could please, untimer my pc shutting down, thank you, your friend %USERNAME% goto finish ELSE goto tryag
+
+
+:finish
+
+shutdown /a
+cls
+echo Haha, congradulations, i'm very proud of you, You've beaten me at my own game!
+pause
+cls
+echo However...
+pause
+cls
+color 04
+echo There are no winners in war, are there %USERNAME%?
+pause
+ipconfig
+exit
+
+pause
+
